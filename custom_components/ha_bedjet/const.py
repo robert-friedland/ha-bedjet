c5f3b44b1ed4299f4947a61ebffa396f0d2caf19
Robert Friedland <robertfriedland1@gmail.com>
2022-10-23 16:26:25 -0400

    another refactor

---
+++ custom_components/ha_bedjet/const.py
@@ -0,0 +1,17 @@
+BEDJET_COMMAND_UUID = '00002004-bed0-0080-aa55-4265644a6574'
+BEDJET_SUBSCRIPTION_UUID = '00002000-bed0-0080-aa55-4265644a6574'
+BEDJET_COMMANDS = {
+    "off": 0x01,
+    "cool": 0x02,
+    "heat": 0x03,
+    "turbo": 0x04,
+    "dry": 0x05,
+    "ext_ht": 0x06,
+    "fan_up": 0x10,
+    "fan_down": 0x11,
+    "temp_up": 0x12,
+    "temp_down": 0x13,
+    "m1": 0x20,
+    "m2": 0x21,
+    "m3": 0x22
+}

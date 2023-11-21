14a078c522391d738b122d2fb4855d882e2d7722
Brandon Phillips <bphillips09@me.com>
2023-11-21 14:18:26 -0600

    Fix for unloading

--- custom_components/bedjet/__init__.py
+++ custom_components/bedjet/__init__.py
@@ -11,7 +11,4 @@ async def async_setup_entry(hass, entry):
 
 async def async_unload_entry(hass, entry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, "climate"):
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, "climate")

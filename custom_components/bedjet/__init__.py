1569d3cbd2d0be594688ad95226084fcf987c764
Brandon Phillips <bphillips09@me.com>
2023-11-21 15:01:19 -0600

    Unload forwarded entry

--- custom_components/bedjet/__init__.py
+++ custom_components/bedjet/__init__.py
@@ -11,4 +11,4 @@ async def async_setup_entry(hass, entry):
 
 async def async_unload_entry(hass, entry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, "climate")
+    return await hass.config_entries.async_forward_entry_unload(entry, "climate")
